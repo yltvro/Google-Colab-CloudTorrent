57b6fecd712f031ee845e70609ce1f172095bc28
yltvro <31468552+yltvro@users.noreply.github.com>
2020-05-16 09:45:43 +0800

    Update doneCMD.sh

--- res/scripts/doneCMD.sh
+++ res/scripts/doneCMD.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-UP="${CLD_DIR}/${CLD_PATH}"
+#UP="${CLD_DIR}/${CLD_PATH}"
 
-if [[ -d ${UP} ]]; then
-  exit 0
-fi
+#if [[ -d ${UP} ]]; then
+#  exit 0
+#fi
 
 #Upload to Gdrive
-mkdir -p "/content/drive/My Drive/$(dirname "${CLD_PATH}")"
-mv "${UP}" "/content/drive/My Drive/${CLD_PATH}"
+#mkdir -p "/content/drive/My Drive/$(dirname "${CLD_PATH}")"
+#mv "${UP}" "/content/drive/My Drive/${CLD_PATH}"
+cp "/content/drive/Shared drives/HomeNet6-peoples/VIDEO/纪录片/一千零一夜的秘密(全1集)/[英国广播公司.一千零一夜的秘密].Secrets.of.the.Arabian.Nights.2010-AVI-夏末秋字幕组.mkv" "/content/drive/Shared drives/yltvro"
